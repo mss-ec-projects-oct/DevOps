4715bfe483d0e0a75a249fb97296925229745eb3
pmmanoj650 <100923361+pmmanoj650@users.noreply.github.com>
2024-11-13 21:42:22 +0530

    Update New_user.sh

--- New_user.sh
+++ New_user.sh
@@ -5,5 +5,6 @@ sudo useradd $user
 sudo passwd $user
 sudo sed -i "48i $user    ALL=(ALL:ALL) ALL" /etc/sudoers
 sudo sed -i 62s/^/#/ /etc/ssh/sshd_config
+sudo systemctl restart ssh
 sudo mkdir /home/$user
 sudo chown $user:$user /home/$user
