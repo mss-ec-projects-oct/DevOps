cad8ac2606bfd96341dae3854b84c74fbfbc4550
pmmanoj650 <100923361+pmmanoj650@users.noreply.github.com>
2024-11-13 07:30:48 +0530

    Create Multi_Table_while.sh

---
+++ Multi_Table_while.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo -e "Please provide one value: \c"
+read -r c
+i=1
+while [ $i -lt 10 ];
+do
+        b=`expr $c \* $i`
+        echo " $c * $i = $b "
+        i=`expr $i + 1`
+done
