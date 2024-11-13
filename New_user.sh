92ff85e02e25c422fc3811c671bb123b6975287f
pmmanoj650 <100923361+pmmanoj650@users.noreply.github.com>
2024-11-13 21:35:20 +0530

    Update New_user.sh

--- New_user.sh
+++ New_user.sh
@@ -3,7 +3,7 @@ echo "please provide a username to create."
 read user
 sudo useradd $user
 sudo passwd $user
-sudo sed -i "50i $user    ALL=(ALL:ALL) ALL" /etc/sudoers
+sudo sed -i "48i $user    ALL=(ALL:ALL) ALL" /etc/sudoers
 sudo sed -i 62s/^/#/ /etc/ssh/sshd_config
 sudo mkdir /home/$user
 sudo chown $user:$user /home/$user
