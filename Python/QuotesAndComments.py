8457d469f609e89720b4efc2ce02875400673a15
MANOJ <pmmanoj1997@gmail.com>
2024-12-04 21:53:04 +0530

    Added QuotesAndComments.py and Varaibles.py on 4dec2024

---
+++ Python/QuotesAndComments.py
@@ -0,0 +1,22 @@
+#This is a single line comment
+"""
+This is a multi-line comment.
+It is double quotes(thrice)
+"""
+'''
+This is a multi-line comment.
+It is single quote(thrice)
+'''
+skill = "DevOps"
+
+print(skill)
+print("skill")
+print('skill')
+
+print("""
+This is a paragraph string, I can write it in multiple lines.
+this is the second line of paragraph
+third line
+""")
+
+#print(this is a string)
---
+++ Python/Variables.py
@@ -0,0 +1,23 @@
+#variable Assignments
+var1 = "Python"
+var2 = 75
+var3 = 3.5
+
+#Printing variables
+print(var1)
+print(var2)
+print(var3)
+
+#Multiple Assignments
+a = b = c = 65
+
+print(a)
+print(b)
+print(c)
+
+#Multiple values assigned to multiple variables
+x, y, z = "alpha", "beta", 5
+
+print("variable x value is", x)
+print("variable y value is", y)
+print("variable z value is", z)
