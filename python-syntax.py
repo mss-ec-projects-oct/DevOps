a19ce5f5b20af8118be82d248fcf5c1fb47665b7
MANOJ <pmmanoj1997@gmail.com>
2024-12-04 12:46:00 +0530

    python

---
+++ python-syntax.py
@@ -0,0 +1,11 @@
+x = 0
+print("Learning Indentation")
+print()
+if x == 0:
+    print("in the if block")
+    print("value of x is 0")
+else:
+    print("in the else block")
+    print("value of x is not 0")
+print()
+print("this statement is out of if/else block.")
