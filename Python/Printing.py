a649362077f58141fb1792dae7bd28ab378e513b
MANOJ <pmmanoj1997@gmail.com>
2024-12-06 20:42:11 +0530

    Adding Printing.py

---
+++ Python/Printing.py
@@ -0,0 +1,13 @@
+name = "sars-cov-2"
+disease = "covid19"
+
+print("The name OS virus is",name)
+
+print("The name of virus is {}".format(name))
+print("{} is the name of virus.".format(name))
+print("The name of the virus is {} and it causes {}".format(name, disease))
+
+print(f"The name of virus is {name} and it causes {disease}")
+
+#Concatination
+print("The name of virus is" + " " + name)
