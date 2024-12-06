20668c9e955749f2b793ee6b82989178cca14b63
MANOJ <pmmanoj1997@gmail.com>
2024-12-06 17:53:04 +0530

    Adding Datatypes.py

---
+++ Python/Datatypes.py
@@ -0,0 +1,39 @@
+#strings
+str1 = "alpha"
+str2 = 'beta'
+str3 = '''gamma string'''
+str4 = """delta string"""
+
+#Numbers
+num1 = 123
+flt1 = 2.2
+
+#List/ collection of multiple datatypes enclosed in square brackets.
+first_list = [str1, "DevOps", 47, num1, 1.5]
+
+#printing a list
+print(first_list)
+
+#Tuple / collection of multiple datatypes enclosed in round brackets.
+first_tuple = (str1, "DevOps", 47, num1, 1.5)
+
+#printing a tuple
+print(first_tuple)
+
+#Dictionary / Elements in the dictionary are always in pairs(key:value), enclosed in curly brackets.
+first_dictionary = {"Name":"Manoj", "Weight":60, "Exercises": ["Boxing","Dancing","Fighting"]}
+
+#printing a Dictionary
+print(first_dictionary)
+
+print("variable first_list is a:", type(first_list))
+print("variable first_tuple is a:", type(first_tuple))
+print("variable first_dictionary is a:", type(first_dictionary))
+
+#Boolean
+X = True
+Y = False
+
+#Printing Boolean
+print(X)
+print("variable Y is a:", type(Y))
