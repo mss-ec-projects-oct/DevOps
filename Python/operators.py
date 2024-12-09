1eae54a8c13c560be2ff4834867c3ac56469d1f6
MANOJ <pmmanoj1997@gmail.com>
2024-12-09 21:29:25 +0530

    Adding operators.py

---
+++ Python/operators.py
@@ -0,0 +1,73 @@
+#Arithmetic Operators
+x = 2
+y = 5
+
+total = x + y
+print(total)
+
+total = x - y
+print(total)
+
+total = x * y
+print(total)
+
+total = y / x
+print(total)
+
+total = y % x
+print(total)
+
+total = y ** x
+print(total)
+
+#comparison operators
+a = 30
+b = 60
+
+out = a < b
+print(out)
+
+out = a > b
+print(out)
+
+out = (a == b)
+print(out)
+
+out = (a != b)
+print(out)
+
+out = (a <= b)
+print(out)
+
+out = (a >= b)
+print(out)
+
+#Assignment Operators
+
+c = 0
+d = 10
+
+c += d  # c = c + d
+print(c)
+
+c -= d  # c = c - d
+print(c)
+
+#Logical Operators
+#and
+#or
+
+a = 40
+b = 60
+
+y = 3
+x = 2
+
+out = (a<b) or (y<x)
+print(out)
+
+out = (a>b) or (x>y)
+print(out)
+
+out = (a<b) and (x<y)
+print(out)
