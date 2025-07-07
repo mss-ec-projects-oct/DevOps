948441c941d29533afde7232816367720d346563
Manoj <manoj@gmail.com>
2025-07-07 14:32:53 +0000

    Reading a Large CSV File Line by Line

---
+++ CSV_Files/1.reading_a_large_csv_file_line_by_line.py
@@ -0,0 +1,8 @@
+#!/bin/python3
+#Scenario: File too large to load into memory at once (e.g., 10GB logs file).
+import csv
+with open('text.csv', 'r') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        #process each row
+        print(row)
